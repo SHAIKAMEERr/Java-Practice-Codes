409166a1b10e7d44297bf4fbb68945e993639504
SHAIK AMEER <shaikameerjann@gmail.com>
2025-03-14 21:58:54 +0530

    Added Singleton and StaticInner classes

--- InnerClasses/src/Singleton.java
+++ InnerClasses/src/Singleton.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Singleton {
+class Singleton
+{
+    static float milk;
+    static float water;
+    static float sugar;
+    static float coffeePowder;
+    private static  Singleton coffeeMachine=null;
+    private Singleton()
+    {
+        milk=10.05f;
+        water=5.55f;
+        sugar=1.23f;
+        coffeePowder=0.750f;
+    }
+    public static Singleton getCoffee()
+    {
+        if(coffeeMachine==null)
+            coffeeMachine=new Singleton();
+        return coffeeMachine;
+    }
 }
--- InnerClasses/src/StaticInner.java
+++ InnerClasses/src/StaticInner.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class StaticInner {
+class StaticInner
+{
+    static int  a=20;
+    int b=20;
+    static class Inner
+    {
+        static int c=20;
+        static void staticSum()
+        {
+            System.out.println(a+c);
+        }
+    }
 }

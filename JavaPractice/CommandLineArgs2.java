33481d4bf4ec4da018366e9fc864702b7a10b123
SHAIK AMEER <shaikameerjann@gmail.com>
2025-03-19 14:08:06 +0530

    Added JavaPractice directory - Contains various Java practice programs

---
+++ JavaPractice/CommandLineArgs2.java
@@ -0,0 +1,12 @@
+package Mypack;
+import java.lang.String;
+class Comp
+{
+public static void main(String args[])
+{
+    java.util.Scanner sc=new java.util.Scanner(System.in);
+    System.out.println("To Practice Package Creation in CMD ");
+    String name=sc.next();
+    System.out.println("Package : "+name);
+}
+}
